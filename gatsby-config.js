d655c1d934a458b78649bdafb7ed7a7a54bdaac5
Alex <alex.lyman2@gmail.com>
2022-03-16 23:35:57 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+    title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": ""
+    }
+  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
